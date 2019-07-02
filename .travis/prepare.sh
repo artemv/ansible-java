eac3fa19bda5f0b8fa20398d2db64d5c8147df8e
Artem Vasiliev <artem.job@gmail.com>
2019-07-02 10:13:30 +0300

    chore(ci): try another env var
    looks like travis decided to wipe JDK_URL because it was secure before

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -1,6 +1,5 @@
 pwd && ls -l . && ls -l ./test/test.yml
-echo "        jdk_url: $JDK_URL" >> ./test/test.yml
+echo "        jdk_url: $MY_JDK_URL" >> ./test/test.yml
 echo "        jvm_zipfile: $JVM_ZIPFILE" >> ./test/test.yml
-echo "$JDK_URL, ${JDK_URL}"
-echo "        jdk_url: $JDK_URL"
+echo "        jdk_url: $MY_JDK_URL"
 cat ./test/test.yml
