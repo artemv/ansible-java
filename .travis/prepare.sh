cf9c9233f2da27e9cd29cdcda9e9b5a081c3ab23
Artem Vasiliev <artem.job@gmail.com>
2019-07-02 10:04:08 +0300

    chore(ci): more debugging

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -1,4 +1,6 @@
 pwd && ls -l . && ls -l ./test/test.yml
 echo "        jdk_url: $JDK_URL" >> ./test/test.yml
 echo "        jvm_zipfile: $JVM_ZIPFILE" >> ./test/test.yml
+echo "$JDK_URL, ${JDK_URL}"
+echo "        jdk_url: $JDK_URL"
 cat ./test/test.yml
